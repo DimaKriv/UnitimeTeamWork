5b418fd56ec30bb0e9d9ba08fa57ea80a32f1282
imgret <imgret@ttu.ee>
2018-04-02 21:18:14 +0300

    update for parserUtility, now it has function for connection to database and function for querying data from database/

--- UnitimeDataParser/src/main/java/Main.java
+++ UnitimeDataParser/src/main/java/Main.java
@@ -1,6 +1,5 @@
 import parserUtility.ParserUtility;
 
-import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,30 +9,9 @@ import java.sql.Statement;
 public class Main {
 
     /**
-     * select all rows in the institudid table
-     */
-    public void selectAll(){
-        String sql = "SELECT name FROM INSTITUDID";
-
-        try (Connection conn = ParserUtility.connectToDatabase();
-             Statement stmt  = conn.createStatement();
-             ResultSet rs    = stmt.executeQuery(sql)){
-
-            // loop through the result set
-            while (rs.next()) {
-                System.out.println(rs.getString("name"));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-
-    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Main app = new Main();
-        app.selectAll();
     }
 }
--- UnitimeDataParser/src/main/java/parserUtility/ParserUtility.java
+++ UnitimeDataParser/src/main/java/parserUtility/ParserUtility.java
@@ -1,24 +1,40 @@
 package parserUtility;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ParserUtility {
 
+    // SQLite connection string
+    public final static String DATABASE_URL = "jdbc:sqlite:../Tunniplaan.db";
+
     /**
-     * Connect to the Tunniplaan.db database
+     * Connect to the database
      * @return the Connection object
      */
     public static Connection connectToDatabase() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:../Tunniplaan.db";
-        Connection conn = null;
+        Connection connection = null;
         try {
-            conn = DriverManager.getConnection(url);
+            connection = DriverManager.getConnection(DATABASE_URL);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return conn;
+        return connection;
+    }
+
+    /**
+     * Make query from database and return queried data in ResultSet object
+     * @param querySQL SQL statement to query from database
+     * @return queried data in ResultSet object.
+     */
+    public ResultSet queryDataFromDatabase(String querySQL){
+        ResultSet resultSet = null;
+        try {
+            Connection connection = connectToDatabase();
+            Statement statement  = connection.createStatement();
+            resultSet = statement.executeQuery(querySQL);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return resultSet;
     }
 }
