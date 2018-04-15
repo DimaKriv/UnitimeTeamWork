1d2f5a00a85d51d36e68441f411e5cbcfdef353c
imgret <imgret@ttu.ee>
2018-04-15 17:34:07 +0300

    deleted broken and unnecessary folder main from UnitimeDataParser/src/main

--- UnitimeDataParser/src/main/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLException;
-
-public class Main {
-    public static void main(String[] arg) throws SQLException {
-        ConnectionSer connection = new ConnectionSer(new FileReader());
-        connection.createConnection(connection.getConsoleReader());
-    }
-}
