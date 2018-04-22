ee3c24fcf5cb17d3bf048ee01810c28cab9678b5
sezare <sezare@ttu.ee>
2018-04-22 22:16:09 +0300

    v2.1 empty test files

---
+++ UnitimeDataParser/src/test/java/toXmlParser/sessionsetup/AcademicSessionSetupTest.java
@@ -0,0 +1,22 @@
+package toXmlParser.sessionsetup;
+
+/**
+ * Created by lll on 22-Apr-18.
+ */
+class AcademicSessionSetupTest {
+    @org.junit.jupiter.api.Test
+    void buildXML() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDateInFormat() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getResultSetDayAndWeek() {
+
+    }
+
+}
---
+++ UnitimeDataParser/src/test/java/toXmlParser/sessionsetup/TimeDatePatternsTest.java
@@ -0,0 +1,19 @@
+package toXmlParser.sessionsetup;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by lll on 22-Apr-18.
+ */
+class TimeDatePatternsTest {
+    @Test
+    void buildTimePatternsXML() {
+
+    }
+
+    @Test
+    void buildDatePatterns() {
+
+    }
+
+}
