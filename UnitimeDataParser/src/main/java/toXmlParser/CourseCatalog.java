4b9a8f972745aa232cd139eabc9d7a3173337df5
nikopa <nikopa@ttu.ee>
2018-04-23 21:15:14 +0300

    Subject Areas test and Parser utility support

--- UnitimeDataParser/src/main/java/toXmlParser/CourseCatalog.java
+++ UnitimeDataParser/src/main/java/toXmlParser/CourseCatalog.java
@@ -9,78 +9,36 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class CourseCatalog {
 
-//    private static final String QUERY_SQL = "SELECT fk_aine_id, ainekood, nimetus_est FROM TUNN_AINE WHERE Length(ainekood)<=10";
-//    private static final ResultSet QUERY_RESULT_SET = ParserUtility.queryDataFromDatabase(QUERY_SQL);
-//
-//    private static final int DEFAULT_FAKE_EAP = 5;
-//
-//    private void printXMLFile(XMLBuilder courseCatalog) throws FileNotFoundException, TransformerException {
-//        new File("XMLFiles").mkdirs();
-//        PrintWriter writer = new PrintWriter(new FileOutputStream("XMLFiles/course_catalog.xml"));
-//        Properties outputProperties = new Properties();
-//
-//        outputProperties.put(javax.xml.transform.OutputKeys.OMIT_XML_DECLARATION, "yes");
-//        outputProperties.put(javax.xml.transform.OutputKeys.INDENT, "yes");
-//        outputProperties.put("{http://xml.apache.org/xslt}indent-amount", "2");
-//        courseCatalog.toWriter(writer, outputProperties);
-//    }
-//
-//    public void buildXML() {
-//        try {
-//            XMLBuilder courseCatalog = XMLBuilder.create("courseCatalog")
-//                    .attribute("campus", "TTU")
-//                    .attribute("term", "Fall")
-//                    .attribute("year", "2018");
-//
-//            while (QUERY_RESULT_SET.next()) {
-//                String subject = QUERY_RESULT_SET.getString("ainekood");
-//                String courseNumber = QUERY_RESULT_SET.getString("fk_aine_id");
-//                String title = QUERY_RESULT_SET.getString("nimetus_est");
-//
-//                XMLBuilder course = courseCatalog.element("course")
-//                        .attribute("subject", subject)
-//                        .attribute("courseNumber", courseNumber)
-//                        .attribute("title", title);
-//
-//                course.element("courseCredit")
-//                        .attribute("creditType", "collegiate")
-//                        .attribute("creditUnitType", "semesterHours")
-//                        .attribute("creditFormat", "fixedUnit")
-//                        .attribute("fixedCredit", String.valueOf(DEFAULT_FAKE_EAP));
-//            }
-//
-//            printXMLFile(courseCatalog);
-//
-//        } catch (ParserConfigurationException | SQLException | FileNotFoundException | TransformerException e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println("Course catalog XML is successfully created");
-//    }
-
     private static final int DEFAULT_FAKE_EAP = 5;
 
+    private ParserUtility utility;
     private String querySql;
     private ResultSet queryResultSet;
 
-    public CourseCatalog() {
+    public CourseCatalog() throws SQLException {
+        utility = new ParserUtility();
+        Connection connection = utility.connectToDatabase();
+        Statement statement = utility.createStatement(connection);
         querySql = "SELECT fk_aine_id, ainekood, nimetus_est FROM TUNN_AINE WHERE Length(ainekood)<=10";
-        queryResultSet = ParserUtility.queryDataFromDatabase(querySql);
+        queryResultSet = utility.queryDataFromDatabase(querySql, statement);
     }
 
     public CourseCatalog(String querySql, ResultSet queryResultSet) {
+        utility = new ParserUtility();
         this.querySql = querySql;
         this.queryResultSet = queryResultSet;
     }
 
     public XMLBuilder createCourseCatalogElementBuilder(String campus, String term, String year) throws ParserConfigurationException {
-        return XMLBuilder.create("departments")
+        return XMLBuilder.create("courseCatalog")
                 .attribute("campus", campus)
                 .attribute("term", term)
                 .attribute("year", year);
@@ -110,7 +68,7 @@ public class CourseCatalog {
     }
 
     public void writeXML(XMLBuilder xmlBuilder) throws FileNotFoundException, TransformerException {
-        ParserUtility.writeToXMLFile(xmlBuilder, "course_catalog.xml");
+        utility.writeToXMLFile(xmlBuilder, "course_catalog.xml");
     }
 
     public void createXMLFile(String campus, String term, String year) {
---
+++ UnitimeDataParser/src/test/java/toXmlParserTests/SubjectAreasTest.java
@@ -0,0 +1,109 @@
+package toXmlParserTests;
+
+import com.jamesmurty.utils.XMLBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import toXmlParser.SubjectAreas;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SubjectAreasTest {
+
+    private String queryMock;
+    private ResultSet queryResultSetMock;
+    private SubjectAreas subjectAreas;
+
+    @Before
+    public void setUp() {
+        this.queryMock = "";
+        this.queryResultSetMock = mock(ResultSet.class);
+        this.subjectAreas = new SubjectAreas(queryMock, queryResultSetMock);
+    }
+
+    @Test
+    public void testXMLBuilderWithGivenCampusAndTermAndYearIsCreatedCorrectly() throws ParserConfigurationException,
+            TransformerException {
+
+        XMLBuilder expectedBuilder = XMLBuilder.create("subjectAreas")
+                .attribute("campus", "TTU")
+                .attribute("term", "Fall")
+                .attribute("year", "2018");
+        XMLBuilder actualBuilder = subjectAreas.createSubjectAreasElementBuilder("TTU", "Fall",
+                "2018");
+        assertEquals(expectedBuilder.asString(), actualBuilder.asString());
+    }
+
+    @Test
+    public void testBuildingXMLWithOneEntryInResultSet() throws ParserConfigurationException, SQLException,
+            TransformerException {
+
+        XMLBuilder expectedBuilder = XMLBuilder.create("subjectAreas")
+                .attribute("campus", "TTU")
+                .attribute("term", "Fall")
+                .attribute("year", "2018");
+
+        expectedBuilder.element("subjectArea")
+                .attribute("externalId", "SA0000000000000000005")
+                .attribute("abbreviation", "INFR")
+                .attribute("title", "Informaatika")
+                .attribute("department", "1001");
+
+        when(queryResultSetMock.next()).thenReturn(true).thenReturn(false);
+        when(queryResultSetMock.getString("EXTERNAL_ID")).thenReturn("SA0000000000000000005");
+        when(queryResultSetMock.getString("ABBV")).thenReturn("INFR");
+        when(queryResultSetMock.getString("TITLE")).thenReturn("Informaatika");
+
+        XMLBuilder actualBuilder = subjectAreas.buildXML("TTU", "Fall", "2018");
+
+        assertEquals(expectedBuilder.asString(), actualBuilder.asString());
+    }
+
+    @Test
+    public void testBuildingXMLWithTwoEntryInResultSet() throws SQLException, ParserConfigurationException,
+            TransformerException {
+
+        XMLBuilder expectedBuilder = XMLBuilder.create("subjectAreas")
+                .attribute("campus", "TTU")
+                .attribute("term", "Fall")
+                .attribute("year", "2018");
+
+        expectedBuilder.element("subjectArea")
+                .attribute("externalId", "SA0000000000000000005")
+                .attribute("abbreviation", "INFR")
+                .attribute("title", "Informaatika")
+                .attribute("department", "1001");
+
+        expectedBuilder.element("subjectArea")
+                .attribute("externalId", "SA0000000000000000006")
+                .attribute("abbreviation", "KUBR")
+                .attribute("title", "Küberturbe tehnoloogiad")
+                .attribute("department", "1002");
+
+        when(queryResultSetMock.next()).thenReturn(true).thenReturn(true).thenReturn(false);
+
+        when(queryResultSetMock.getString("EXTERNAL_ID"))
+                .thenReturn("SA0000000000000000005")
+                .thenReturn("SA0000000000000000006");
+
+        when(queryResultSetMock.getString("ABBV"))
+                .thenReturn("INFR")
+                .thenReturn("KUBR");
+
+        when(queryResultSetMock.getString("TITLE"))
+                .thenReturn("Informaatika")
+                .thenReturn("Küberturbe tehnoloogiad");
+
+        XMLBuilder actualBuilder = subjectAreas.buildXML("TTU", "Fall", "2018");
+
+        assertEquals(expectedBuilder.asString(), actualBuilder.asString());
+    }
+
+
+}
