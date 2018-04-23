59737e6e619bb78dae6be80ed4731231561b111b
sezare <sezare@ttu.ee>
2018-04-23 22:28:37 +0300

    v2.2 refactored for new parser utility

---
+++ UnitimeDataParser/src/test/java/toXmlParserTests/sessionsetup/ExaminationPeriodsTest.java
@@ -0,0 +1,19 @@
+package toXmlParserTests.sessionsetup;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by lll on 22-Apr-18.
+ */
+class ExaminationPeriodsTest {
+    @Test
+    void buildExaminationPeriods1() {
+
+    }
+
+    @Test
+    void buildExaminationPeriods() {
+
+    }
+
+}
