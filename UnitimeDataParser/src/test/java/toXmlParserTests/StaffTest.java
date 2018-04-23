8c122820389d8d8bd4706250fa57c27c3e35e62b
dmgolo <dmgolo@ttu.ee>
2018-04-23 23:57:19 +0300

    Created tests for staff parser

---
+++ UnitimeDataParser/src/test/java/toXmlParserTests/StaffTest.java
@@ -0,0 +1,41 @@
+package toXmlParserTests;
+
+import com.jamesmurty.utils.XMLBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import toXmlParser.Departments;
+import toXmlParser.StaffParser;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class StaffTest {
+
+    private StaffParser staffParser;
+    private String queryMock;
+    private ResultSet queryResultSetMock;
+
+    @Before
+    public void setup() {
+        queryMock = "";
+        queryResultSetMock = mock(ResultSet.class);
+        staffParser = new StaffParser(queryMock, queryResultSetMock);
+    }
+
+    @Test
+    public void testXMLBuilderWithGivenCampusAndTermAndYearIsCreatedCorrectly() throws ParserConfigurationException, TransformerException {
+        XMLBuilder expectedBuilder = XMLBuilder.create("staff")
+                .attribute("campus", "TTU")
+                .attribute("term", "Fall")
+                .attribute("year", "2018");
+        XMLBuilder actualBuilder = staffParser.createStaffParserBuilder("TTU", "Fall", "2018");
+        assertEquals(expectedBuilder.asString(), actualBuilder.asString());
+    }
+}
