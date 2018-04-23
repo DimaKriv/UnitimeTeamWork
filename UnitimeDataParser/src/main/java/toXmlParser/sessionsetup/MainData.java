a7883ff47e550deba7404242c2848518f5d42834
sezare <sezare@ttu.ee>
2018-04-23 12:15:50 +0300

    v2.2 refactored for testing. TODO: write tests for classes related with session setup

---
+++ UnitimeDataParser/src/main/java/toXmlParser/sessionsetup/MainData.java
@@ -0,0 +1,39 @@
+package toXmlParser.sessionsetup;
+
+
+import com.jamesmurty.utils.XMLBuilder;
+
+public class MainData {
+    AcademicSessionSetup academicSessionSetup;
+
+    MainData(AcademicSessionSetup academicSessionSetup){
+        this.academicSessionSetup = academicSessionSetup;
+    }
+
+
+
+    void buildXML(){
+
+
+        XMLBuilder session = academicSessionSetup.xmlSessionSetup.element("session")
+                .attribute("startDate", academicSessionSetup.getDateInFormat(0, 0))
+                .attribute("endDate", academicSessionSetup.getDateInFormat(4, 18))
+                .attribute("classEndDate", academicSessionSetup.getDateInFormat(4, 15))
+                .attribute("examStartDate", academicSessionSetup.getDateInFormat(0, 16))
+                .attribute("eventStartDate", academicSessionSetup.getDateInFormat(0, -1))
+                .attribute("eventEndDate", academicSessionSetup.getDateInFormat(1, -1));
+
+
+//            holidays/managers are not important
+//            XMLBuilder holidays = session.element("holidays");
+//            XMLBuilder holiday = holidays.element("holiday")
+//                    .attribute("date", "2118/9/6");
+
+
+
+    }
+
+
+
+
+}
