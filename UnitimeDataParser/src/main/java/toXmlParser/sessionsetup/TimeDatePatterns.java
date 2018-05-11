2b3d30025d1afc261325f3b1c5675685ef4feed7
sezare <sezare@ttu.ee>
2018-05-11 13:55:56 +0300

    added new date patterns

--- UnitimeDataParser/src/main/java/toXmlParser/sessionsetup/TimeDatePatterns.java
+++ UnitimeDataParser/src/main/java/toXmlParser/sessionsetup/TimeDatePatterns.java
@@ -98,6 +98,9 @@ public class TimeDatePatterns {
         datePatterns.importXMLBuilder(addEvenWeeks());
         datePatterns.importXMLBuilder(addWeeksFromOneToEight());
         datePatterns.importXMLBuilder(addWeeksFromNineToSixteen());
+        datePatterns.importXMLBuilder(addWeeksFirstFour());
+        datePatterns.importXMLBuilder(addWeeksLastFour());
+
         return datePatterns;
 
     }
@@ -137,6 +140,7 @@ public class TimeDatePatterns {
                     .attribute("fromDate", academicSessionSetup.getDateInFormat(0, i))
                     .attribute("toDate", academicSessionSetup.getDateInFormat(4, i));
         }
+
 
 
         return datePattern;
@@ -208,5 +212,38 @@ public class TimeDatePatterns {
         }
         return weeksFromEightToSixteen;
     }
+
+    public XMLBuilder addWeeksFirstFour() throws ParserConfigurationException {
+
+        XMLBuilder firstFourWeeks = XMLBuilder.create("datePattern")
+                .attribute("name", "first 4")
+                .attribute("type", "Standard")
+                .attribute("visible", "true")
+                .attribute("default", "true");
+        for (int i = 0; i < 3; i++) {
+            firstFourWeeks.element("dates")
+                    .attribute("fromDate", academicSessionSetup.getDateInFormat(0, i))
+                    .attribute("toDate", academicSessionSetup.getDateInFormat(4, i));
+
+        }
+        return firstFourWeeks;
+    }
+    public XMLBuilder addWeeksLastFour() throws ParserConfigurationException {
+
+        XMLBuilder lastFourWeeks = XMLBuilder.create("datePattern")
+                .attribute("name", "weeks 9-16")
+                .attribute("type", "Standard")
+                .attribute("visible", "true")
+                .attribute("default", "true");
+        for (int i = 13; i < 16; i++) {
+            lastFourWeeks.element("dates")
+                    .attribute("fromDate", academicSessionSetup.getDateInFormat(0, i))
+                    .attribute("toDate", academicSessionSetup.getDateInFormat(4, i));
+
+        }
+        return lastFourWeeks;
+    }
+
+
 
 }
