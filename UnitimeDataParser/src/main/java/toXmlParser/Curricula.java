b48c0673fe56db0fd2f0d8e75b9aa8b768c0c02d
nikopa96 <jertyk@gmail.com>
2018-05-20 18:22:29 +0300

    Curricula refactoring and curricula tests

--- UnitimeDataParser/src/main/java/toXmlParser/Curricula.java
+++ UnitimeDataParser/src/main/java/toXmlParser/Curricula.java
@@ -17,6 +17,13 @@ public class Curricula {
     private String querySql;
     private ResultSet queryResultSet;
 
+    private XMLBuilder xmlBuilder;
+    private int classificationEnrollmentNumber = 0;
+    private String previousCurriculumName = "undefined";
+    private int previousSemester = 0;
+    private int curriculumNumber = 0;
+    private int choiceSubjectCounter = 0;
+
     public Curricula() throws SQLException {
         utility = new ParserUtility();
         Connection connection = utility.connectToDatabase();
@@ -25,92 +32,97 @@ public class Curricula {
         queryResultSet = utility.queryDataFromDatabase(querySql, statement);
     }
 
-    public Curricula(ParserUtility utility, String querySql, ResultSet queryResultSet) {
-        this.utility = utility;
+    public Curricula(String querySql, ResultSet queryResultSet) {
+        utility = new ParserUtility();
         this.querySql = querySql;
         this.queryResultSet = queryResultSet;
     }
 
-    public XMLBuilder createCurriculaElementBuilder(String campus, String term, String year) throws ParserConfigurationException {
-        XMLBuilder xmlBuilder =
-                XMLBuilder.create("curricula")
-                        .attribute("campus", campus)
-                        .attribute("term", term)
-                        .attribute("year", year);
-
-        return xmlBuilder;
-    }
-
     public XMLBuilder buildXML(String campus, String term, String year) throws ParserConfigurationException, SQLException {
-        XMLBuilder xmlBuilder = createCurriculaElementBuilder(campus, term, year);
-
-        int classificationEnrollmentNumber = 0;
-        String previousCurriculumName = "undefined";
-        int previousSemester = 0;
-        int curriculumNumber = 0;
-        int choiceSubjectCounter = 0;
+        xmlBuilder = createCurriculaElementBuilder(campus, term, year);
 
         while (queryResultSet.next()) {
-
             String currentCurriculumName = queryResultSet.getString("peaeriala_nimetus");
             int currentSemester = Integer.parseInt(queryResultSet.getString("semester_number"));
             String subjectName = queryResultSet.getString("ainekood");
             String subjectType = queryResultSet.getString("aine_tyyp");
 
             if (!previousCurriculumName.equals(currentCurriculumName)) {
-                classificationEnrollmentNumber++;
-                curriculumNumber++;
-                if (curriculumNumber > 1) {
-                    xmlBuilder = xmlBuilder.up().up();
-                }
-                xmlBuilder =  xmlBuilder.element("curriculum")
-                        .attribute("name", currentCurriculumName)
-                        .element("academicArea")
-                        .attribute("abbreviation", "TTUM")
-                        .up();
-                previousCurriculumName = currentCurriculumName;
-                choiceSubjectCounter = 0;
+                printCurriculumElement(currentCurriculumName);
             }
+
             if (previousSemester != currentSemester) {
-                if (currentSemester > 1) {
-                    xmlBuilder = xmlBuilder.up();
-                }
-                xmlBuilder =  xmlBuilder.element("classification")
-                        .attribute("enrollment", String.valueOf(classificationEnrollmentNumber))
-                        .element("academicClassification")
-                        .attribute("code", String.valueOf(currentSemester))
-                        .up();
-                previousSemester = currentSemester;
-                choiceSubjectCounter++;
+                printClassificationElement(currentSemester);
             }
+
             if (previousCurriculumName.equals(currentCurriculumName)) {
-                xmlBuilder = xmlBuilder.element("course")
-                        .attribute("subject", subjectName)
-                        .attribute("courseNbr", "1");
-
-                if (subjectType.equals("V")) {
-                    xmlBuilder.element("group")
-                            .attribute("id", String.valueOf(choiceSubjectCounter))
-                            .attribute("type", "OPT");
-                }
-
-                xmlBuilder = xmlBuilder.up();
-
+                printCourseElement(subjectName, subjectType);
             }
-
         }
 
         return xmlBuilder;
     }
 
+    public XMLBuilder createCurriculaElementBuilder(String campus, String term, String year) throws ParserConfigurationException {
+        XMLBuilder xmlDocument =
+                XMLBuilder.create("curricula")
+                        .attribute("campus", campus)
+                        .attribute("term", term)
+                        .attribute("year", year);
+
+        return xmlDocument;
+    }
+
+    private void printCurriculumElement(String currentCurriculumName) {
+        classificationEnrollmentNumber++;
+        curriculumNumber++;
+        if (curriculumNumber > 1) {
+            xmlBuilder = xmlBuilder.up().up();
+        }
+        xmlBuilder =  xmlBuilder.element("curriculum")
+                .attribute("name", currentCurriculumName)
+                .element("academicArea")
+                .attribute("abbreviation", "TTUM")
+                .up();
+        previousCurriculumName = currentCurriculumName;
+        choiceSubjectCounter = 0;
+    }
+
+    private void printClassificationElement(int currentSemester) {
+        if (currentSemester > 1) {
+            xmlBuilder = xmlBuilder.up();
+        }
+        xmlBuilder =  xmlBuilder.element("classification")
+                .attribute("enrollment", String.valueOf(classificationEnrollmentNumber))
+                .element("academicClassification")
+                .attribute("code", String.valueOf(currentSemester))
+                .up();
+        previousSemester = currentSemester;
+        choiceSubjectCounter++;
+    }
+
+    private void printCourseElement(String subjectName, String subjectType) {
+        xmlBuilder = xmlBuilder.element("course")
+                .attribute("subject", subjectName)
+                .attribute("courseNbr", "1");
+
+        if (subjectType.equals("V")) {
+            xmlBuilder.element("group")
+                    .attribute("id", String.valueOf(choiceSubjectCounter))
+                    .attribute("type", "OPT");
+        }
+
+        xmlBuilder = xmlBuilder.up();
+    }
+
     public void writeXML(XMLBuilder xmlBuilder) throws FileNotFoundException, TransformerException {
-        utility.writeToXMLFile(xmlBuilder, "curricula.xml");
+        utility.writeToXMLFile(xmlBuilder, "curricula_refactored.xml");
     }
 
     public void createXMLFile(String campus, String term, String year) {
         try {
-            XMLBuilder xmlBuilder = buildXML(campus, term, year);
-            writeXML(xmlBuilder);
+            XMLBuilder xmlDocument = buildXML(campus, term, year);
+            writeXML(xmlDocument);
         } catch (ParserConfigurationException | SQLException | FileNotFoundException | TransformerException e) {
             e.printStackTrace();
         }
--- UnitimeDataParser/src/test/java/toXmlParser/CurriculaTest.java
+++ UnitimeDataParser/src/test/java/toXmlParser/CurriculaTest.java
@@ -1,4 +1,113 @@
 package toXmlParser;
 
+import com.jamesmurty.utils.XMLBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class CurriculaTest {
+
+    private String queryMock;
+    private ResultSet queryResultSetMock;
+    private Curricula curricula;
+
+    @Before
+    public void setUp() {
+        this.queryMock = "";
+        this.queryResultSetMock = mock(ResultSet.class);
+        this.curricula = new Curricula(queryMock, queryResultSetMock);
+    }
+
+    @Test
+    public void testXMLBuilderWithGivenCampusAndTermAndYearIsCreatedCorrectly() throws ParserConfigurationException,
+            TransformerException {
+
+        XMLBuilder expectedBuilder = XMLBuilder.create("curricula")
+                .attribute("campus", "TTU")
+                .attribute("term", "Fall")
+                .attribute("year", "2018");
+        XMLBuilder actualBuilder = curricula.createCurriculaElementBuilder("TTU", "Fall",
+                "2018");
+        assertEquals(expectedBuilder.asString(), actualBuilder.asString());
+    }
+
+    @Test
+    public void testBuildingXMLWithOneEntryInResultSet() throws ParserConfigurationException, SQLException, TransformerException {
+        XMLBuilder expectedBuilder = XMLBuilder.create("curricula")
+                .attribute("campus", "TTU")
+                .attribute("term", "Fall")
+                .attribute("year", "2018");
+
+        XMLBuilder curriculum = expectedBuilder.element("curriculum")
+                .attribute("name", "Elektroonika ja telekommunikatsioon");
+
+        curriculum.element("academicArea")
+                .attribute("abbreviation", "TTUM");
+
+        XMLBuilder classification = curriculum.element("classification")
+                .attribute("enrollment", "1");
+
+        classification.element("academicClassification")
+                .attribute("code", "1");
+
+        classification.element("course")
+                .attribute("courseNbr", "1")
+                .attribute("subject", "ITI0101");
+
+
+        when(queryResultSetMock.next()).thenReturn(true).thenReturn(false);
+        when(queryResultSetMock.getString("peaeriala_nimetus")).thenReturn("Elektroonika ja telekommunikatsioon");
+        when(queryResultSetMock.getString("semester_number")).thenReturn("1");
+        when(queryResultSetMock.getString("ainekood")).thenReturn("ITI0101");
+        when(queryResultSetMock.getString("aine_tyyp")).thenReturn("K");
+
+        XMLBuilder actualBuilder = curricula.buildXML("TTU", "Fall", "2018");
+        assertEquals(expectedBuilder.asString(), actualBuilder.asString());
+    }
+
+    @Test
+    public void testBuildingXMLWithOneEntryInResultSetWithGroup() throws ParserConfigurationException, SQLException, TransformerException {
+        XMLBuilder expectedBuilder = XMLBuilder.create("curricula")
+                .attribute("campus", "TTU")
+                .attribute("term", "Fall")
+                .attribute("year", "2018");
+
+        XMLBuilder curriculum = expectedBuilder.element("curriculum")
+                .attribute("name", "Elektroonika ja telekommunikatsioon");
+
+        curriculum.element("academicArea")
+                .attribute("abbreviation", "TTUM");
+
+        XMLBuilder classification = curriculum.element("classification")
+                .attribute("enrollment", "1");
+
+        classification.element("academicClassification")
+                .attribute("code", "1");
+
+        XMLBuilder course = classification.element("course")
+                .attribute("courseNbr", "1")
+                .attribute("subject", "ITI0101");
+
+        course.element("group")
+                .attribute("id", "1")
+                .attribute("type", "OPT");
+
+        when(queryResultSetMock.next()).thenReturn(true).thenReturn(false);
+        when(queryResultSetMock.getString("peaeriala_nimetus")).thenReturn("Elektroonika ja telekommunikatsioon");
+        when(queryResultSetMock.getString("semester_number")).thenReturn("1");
+        when(queryResultSetMock.getString("ainekood")).thenReturn("ITI0101");
+        when(queryResultSetMock.getString("aine_tyyp")).thenReturn("V");
+
+        XMLBuilder actualBuilder = curricula.buildXML("TTU", "Fall", "2018");
+        assertEquals(expectedBuilder.asString(), actualBuilder.asString());
+    }
+
 }
