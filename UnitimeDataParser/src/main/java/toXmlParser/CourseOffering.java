cef916a16d02d5be744d2061ba8b046d690364f1
Dmitri.Krivobokov <dmitri.krivobokov@ttu.ee>
2018-05-31 23:19:05 +0300

    This branch contain undocument part, which is differnt, that classe have instructors some do not have instructor

--- UnitimeDataParser/src/main/java/toXmlParser/CourseOffering.java
+++ UnitimeDataParser/src/main/java/toXmlParser/CourseOffering.java
@@ -1,16 +1,18 @@
 package toXmlParser;
 
 import com.jamesmurty.utils.XMLBuilder;
+import javafx.util.Pair;
 import parserUtility.ParserUtility;
 import toXmlParser.dataOptimization.ClassOptimization;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.FileNotFoundException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class CourseOffering {
 
@@ -22,7 +24,10 @@ public class CourseOffering {
         utility = new ParserUtility();
         Connection connection = utility.connectToDatabase();
         Statement statement = utility.createStatement(connection);
-        querySql = "select inimeste_arv,ainekood,nimetus,loeng,praktikum,harjutus from U_COURSEOFFERING";
+        querySql ="select inimeste_arv,ainekood,nimetus,loeng,praktikum,harjutus,T.oppejoud_id,T.type " +
+                "from U_COURSEOFFERING u left join (select distinct AO.aine_id, AO.oppejoud_id,AT.type " +
+                "from aine_type_oppejoud at inner join aine_oppejoud ao on ao.aine_id=at.aine_id " +
+                "and ao.oppejoud_id=at.oppejoud_id)t on t.aine_id=u.fk_aine_id;";
         queryResultSet = utility.queryDataFromDatabase(querySql, statement);
     }
 
@@ -44,6 +49,17 @@ public class CourseOffering {
     public XMLBuilder buildXML(String campus, String term, String year) throws ParserConfigurationException, SQLException {
         XMLBuilder xmlBuilder = createCourseOfferingElementBuilder(campus, term, year);
         int i = 1;
+        String curlimitStr = null;
+        String cursubject = null;
+        String curcourseNumber = null;
+        String curlecture = null;
+        String curpaktikum = null;
+        String curharjutus = null;
+        HashMap<Integer,List<Integer>> staffSortByClassCode = new HashMap<>();
+        int loengINT = 0; int praktikumINT = 1; int harjutusINT = 2;
+        staffSortByClassCode.put(loengINT,new ArrayList<>());
+        staffSortByClassCode.put(praktikumINT,new ArrayList<>());
+        staffSortByClassCode.put(harjutusINT,new ArrayList<>());
         while (queryResultSet.next()) {
             String limitStr = queryResultSet.getString("inimeste_arv");
             String subject = queryResultSet.getString("nimetus");
@@ -51,40 +67,41 @@ public class CourseOffering {
             String lecture = queryResultSet.getString("loeng");
             String paktikum = queryResultSet.getString("praktikum");
             String harjutus = queryResultSet.getString("harjutus");
-            ClassOptimization optimizator = new ClassOptimization();
-            int[][] subpartMin = optimizator.makeCourceOfferringData(
-                    optimizator.makeIntMassiveFromStringInput(lecture, paktikum, harjutus));
-            int limit;
-            try {
-                limit = Integer.parseInt(limitStr);
-            } catch (Exception e) {
-                System.out.println("Not valid limit");
+            String instructorId = queryResultSet.getString("oppejoud_id");
+            String lessonType = queryResultSet.getString("type");
+            if (cursubject == null) {
+                curlimitStr = limitStr;
+                cursubject = subject;
+                curcourseNumber = courseNumber;
+                curlecture = lecture;
+                curpaktikum = paktikum;
+                curharjutus = harjutus;
+                putToMapTypeOfClassAndInstructorId(staffSortByClassCode, lessonType,instructorId);
                 continue;
             }
-            XMLBuilder out = xmlBuilder.element("offering")
-                    .attribute("id", String.valueOf(i++))
-                    .attribute("offered", "true")
-                    .attribute("action", "insert|update|delete")
-                    .element("course")
-                    .attribute("subject", courseNumber)
-                    .attribute("courseNbr", 1 + "")
-                    .attribute("controlling", "true")
-                    .attribute("title", subject)
-                    .element("courseCredit")
-                    .attribute("creditType", "collegiate")
-                    .attribute("creditUnitType", "semesterHours")
-                    .attribute("creditFormat", "creditFormat")
-                    .attribute("fixedCredit", "2")
-                    .up()
-                    .up()
-                    .element("config")
-                    .attribute("name", "1")
-                    .attribute("limit", limit + "");
-            createCourseSubject(subpartMin, out);
-            createCourseClass(subpartMin, out, limit);
+            if (cursubject != null && cursubject.equals(subject)) {
+                putToMapTypeOfClassAndInstructorId(staffSortByClassCode, lessonType,instructorId);
+                continue;
+            } else {
+                addCourse(xmlBuilder, curlimitStr,curlecture,
+                        curpaktikum, curharjutus,cursubject, curcourseNumber, i, staffSortByClassCode);
+                staffSortByClassCode.get(loengINT).clear();
+                staffSortByClassCode.get(praktikumINT).clear();
+                staffSortByClassCode.get(harjutusINT).clear();
+                curlimitStr = limitStr;
+                cursubject = subject;
+                curcourseNumber = courseNumber;
+                curlecture = lecture;
+                curpaktikum = paktikum;
+                curharjutus = harjutus;
+                putToMapTypeOfClassAndInstructorId(staffSortByClassCode, lessonType,instructorId);
+            }
         }
+        addCourse(xmlBuilder, curlimitStr,curlecture,
+                curpaktikum, curharjutus,cursubject, curcourseNumber, i, staffSortByClassCode);
         return xmlBuilder;
     }
+
 
     public void writeXML(XMLBuilder xmlBuilder) throws FileNotFoundException, TransformerException {
         utility.writeToXMLFile(xmlBuilder, "courseOfferings.xml");
@@ -112,7 +129,7 @@ public class CourseOffering {
         }
     }
 
-    public void createCourseClass(int[][] input, XMLBuilder out, int people) {
+    public void createCourseClass(int[][] input, XMLBuilder out, int people, HashMap<Integer, List<Integer>> staff) {
         String[] type = new String[]{"Lec", "Lab", "Rec"};
         int peopleNotInClass = people;
         int suffixOfClass = 1;
@@ -129,7 +146,11 @@ public class CourseOffering {
             out = out.element("class")
                     .attribute("type", type[i]).attribute("suffix"
                             , suffixOfClass + "")
-                    .attribute("limit", limitClassCapacity + "").up();
+                    .attribute("limit", limitClassCapacity + "");
+            for (int j = 0; j < staff.get(i).size(); j++) {
+              out = out.element("instructor").attribute("id",staff.get(i).get(j) + "").up();
+            }
+                   out = out.up();
             if ((peopleNotInClass - limitClassCapacity) > 0) {
                 peopleNotInClass -= limitClassCapacity;
                 suffixOfClass++;
@@ -141,15 +162,70 @@ public class CourseOffering {
         }
     }
 
+    public boolean putToMapTypeOfClassAndInstructorId(HashMap<Integer,List<Integer>> staffSortByCode,
+                                                  String typeOfClass, String strinstructor_id) {
+        Pair<String,Integer>[] typetoCode  = new Pair[] {
+                new Pair("loeng",0), new Pair("praktikum",1), new Pair("harjutus",2)};
+        try {
+            String[] typeMassive = typeOfClass.split("\\+");
+            int instructorId = Integer.parseInt(strinstructor_id);
+            for (String type : typeMassive) {
+                int typeClass = Arrays.asList(typetoCode).stream()
+                        .filter(pair -> type.equals(pair.getKey())).findFirst().get().getValue();
+                staffSortByCode.get(typeClass).add(instructorId);
+            }
+        } catch (Exception exp) {
+            System.out.println(exp.getMessage());
+            return false;
+        }
+        return true;
+    }
+
     public int minPerWeek(int[] classConfig, ClassOptimization op) {
         int[] numberAndMinutesOfClass = op.getNumberAndMinutesOfClass(classConfig);
         return numberAndMinutesOfClass[0] * numberAndMinutesOfClass[1];
     }
 
     public boolean isExist(int[] classConfig, ClassOptimization op) {
-        if (op.getDatePattern(classConfig).equals(ClassOptimization.NO_DATE_PATTERN)
-                || op.getTimePattern(classConfig).equals(ClassOptimization.NO_TIME_PATTERN))
-            return false;
-        else return true;
+        return !op.getDatePattern(classConfig).equals(ClassOptimization.NO_DATE_PATTERN)
+                && !op.getTimePattern(classConfig).equals(ClassOptimization.NO_TIME_PATTERN);
     }
-}
+
+    public void addCourse(XMLBuilder xmlBuilder, String limitStr, String lecture,
+                          String paktikum, String harjutus, String subject, String courseNumber, int i,
+                          HashMap<Integer,List<Integer>> staff) {
+
+        ClassOptimization optimizator = new ClassOptimization();
+        int[][] subpartMin = optimizator.makeCourceOfferringData(
+                optimizator.makeIntMassiveFromStringInput(lecture, paktikum, harjutus));
+        int limit;
+        try {
+            limit = Integer.parseInt(limitStr);
+        } catch (Exception e) {
+            System.out.println("Not valid limit");
+            return ;
+        }
+        XMLBuilder out = xmlBuilder.element("offering")
+                .attribute("id", String.valueOf(i++))
+                .attribute("offered", "true")
+                .attribute("action", "insert|update|delete")
+                .element("course")
+                .attribute("subject", courseNumber)
+                .attribute("courseNbr", 1 + "")
+                .attribute("controlling", "true")
+                .attribute("title", subject)
+                .element("courseCredit")
+                .attribute("creditType", "collegiate")
+                .attribute("creditUnitType", "semesterHours")
+                .attribute("creditFormat", "creditFormat")
+                .attribute("fixedCredit", "2")
+                .up()
+                .up()
+                .element("config")
+                .attribute("name", "1")
+                .attribute("limit", limit + "");
+        createCourseSubject(subpartMin, out);
+        createCourseClass(subpartMin, out, limit, staff);
+    }
+    }
+
--- UnitimeDataParser/src/test/java/toXmlParser/CourseOfferingTest.java
+++ UnitimeDataParser/src/test/java/toXmlParser/CourseOfferingTest.java
@@ -1,15 +1,14 @@
-package toXmlParserTests;
-
+package toXmlParser;
 
 import com.jamesmurty.utils.XMLBuilder;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import toXmlParser.CourseOffering;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.sql.ResultSet;
-import java.util.Properties;
+import java.util.*;
 
 import static org.mockito.Mockito.mock;
 
@@ -23,13 +22,75 @@ public class CourseOfferingTest {
             queryResultSetMock = mock(ResultSet.class);
             courseOffering = new CourseOffering(queryMock, queryResultSetMock);
         }
+
+   @Test
+    public void putToMapTypeOfClassAndInstructorId() {
+       HashMap<Integer, List<Integer>> exp = new HashMap<>();
+       exp.put(0, new ArrayList<>(Arrays.asList(new Integer(0001))));
+       HashMap<Integer, List<Integer>> act = new HashMap<>();
+       act.put(0, new ArrayList<>());
+            courseOffering.putToMapTypeOfClassAndInstructorId(act,"loeng","0001");
+       Assert.assertEquals(act,exp);
+   }
+
+    @Test
+    public void putToMapTypeOfClassAndInstructorId2() {
+        HashMap<Integer, List<Integer>> exp = new HashMap<>();
+        exp.put(0, new ArrayList<>(Arrays.asList(new Integer(0001))));
+        exp.put(1, new ArrayList<>(Arrays.asList(new Integer(0001))));
+        HashMap<Integer, List<Integer>> act = new HashMap<>();
+        act.put(0, new ArrayList<>());
+        act.put(1, new ArrayList<>());
+        courseOffering.putToMapTypeOfClassAndInstructorId(act,"loeng+praktikum","0001");
+        Assert.assertEquals(act,exp);
+    }
+
+    @Test
+    public void putToMapTypeOfClassAndInstructorId3() {
+        HashMap<Integer, List<Integer>> exp = new HashMap<>();
+        exp.put(0, new ArrayList<>(Arrays.asList(new Integer(0001))));
+        exp.put(1, new ArrayList<>(Arrays.asList(new Integer(0001))));
+        exp.put(2,new ArrayList<>((Arrays.asList(new Integer(0001)))));
+        HashMap<Integer, List<Integer>> act = new HashMap<>();
+        act.put(0, new ArrayList<>());
+        act.put(1, new ArrayList<>());
+        act.put(2, new ArrayList<>());
+        courseOffering.putToMapTypeOfClassAndInstructorId(
+                act,"loeng+praktikum+harjutus","0001");
+        Assert.assertEquals(act,exp);
+    }
+    @Test
+    public void putToMapTypeOfClassAndInstructorIdNotValid() {
+        HashMap<Integer, List<Integer>> exp = new HashMap<>();
+        exp.put(0, new ArrayList<>());
+        HashMap<Integer, List<Integer>> act = new HashMap<>();
+        act.put(0, new ArrayList<>());
+        courseOffering.putToMapTypeOfClassAndInstructorId(
+                act,"loeng+praktikum+harjutus","0001d");
+        Assert.assertEquals(act,exp);
+    }
+    @Test
+    public void putToMapTypeOfClassAndInstructorIdNotValidType() {
+        HashMap<Integer, List<Integer>> exp = new HashMap<>();
+        exp.put(0, new ArrayList<>());
+        HashMap<Integer, List<Integer>> act = new HashMap<>();
+        act.put(0, new ArrayList<>());
+        courseOffering.putToMapTypeOfClassAndInstructorId(
+                act,"loengt+praktikum+harjutus","0001");
+        Assert.assertEquals(act,exp);
+    }
+
     @Test
     public void controllStructureOfXmlMannually() {
             int[][] subpartMin =  new int[][] {new int[]{170,30,0},new int[]{0,0,0},new int[]{0,0,200}};
         try {
+            HashMap<Integer,List<Integer>> staff = new HashMap<>();
+            staff.put(0,new ArrayList(Arrays.asList(new String[] {"890", "1000","3020"})));
+            staff.put(1,new ArrayList(Arrays.asList(new String[] {"891", "1001","3021"})));
+            staff.put(2,new ArrayList(Arrays.asList(new String[] {"890", "1002","30022"})));
             XMLBuilder SubpartAndClass = XMLBuilder.create("offering");
                 courseOffering.createCourseSubject(subpartMin, SubpartAndClass);
-                courseOffering.createCourseClass(subpartMin, SubpartAndClass, 100);
+                courseOffering.createCourseClass(subpartMin, SubpartAndClass, 100, staff);
             Properties prop = new Properties();
             prop.put(javax.xml.transform.OutputKeys.INDENT, "yes");
             System.out.println( SubpartAndClass.asString(prop));
@@ -39,4 +100,5 @@ public class CourseOfferingTest {
             e.printStackTrace();
         }
     }
+
 }
